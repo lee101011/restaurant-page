60268b7c673e0bb9223ad210c37f0ab2ca574ff1
Full Name <me@Box.DomainName>
2024-11-15 15:10:27 -0600

    Project setup

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+// npm install -D webpack webpack-cli
+
+// npm install -D webpack-dev-server
+// npx webpack serve -> these two commands replace npx webpack on every change (changes to config require restart)
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "eval-source-map", // displays source files rather than bundled files
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html", /// html-webpack-plugin
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"], // style-loader css-loader
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i, // for when we need to import an image in js
+        type: "asset/resource",
+      },
+    ],
+  },
+};
